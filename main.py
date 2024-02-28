7d5cb91ed270f84b820c46ba79433463c36ccf60
Seth Creasman <sethcreasman@gmail.com>
2024-02-28 11:29:25 -0800

    Update main.py
    added clear_cell function

--- main.py
+++ main.py
@@ -11,9 +11,30 @@ client = pygsheets.authorize(service_account_file="/Users/sethcreasman/src/gcp/a
 spreadsht = client.open("auto_budget_sheet")
 worksht = spreadsht.worksheet("title", "Transactions")
 
+# Cell Values
+# expense_value =
+# expense_comment =
+# expense_category =
 
-# #single cell value update for date amount and comment columns
-update_date = worksht.cell("B6").set_text_format("bold", False).value = today.strftime('%d, %b %Y')
-update_value = worksht.cell("C6").set_text_format("bold", False).value = "2650"
-update_comment = worksht.cell("D6").set_text_format("bold", False).value = "I updated this with Python"
-update_category = worksht.cell("E6").set_text_format("bold", False).value = "House Payment"
+def update_cells():
+    # #single cell value update for date amount and comment columns
+    update_date = worksht.cell("B5").set_text_format("bold", False).value = today.strftime('%d, %b %Y')
+    update_value = worksht.cell("C5").set_text_format("bold", False).value = "5"
+    update_comment = worksht.cell("D5").set_text_format("bold", False).value = "I updated this with Python"
+    update_category = worksht.cell("E5").set_text_format("bold", False).value = "House Payment"
+
+    print("added to cells")
+
+def clear_cell_updates():
+    list_of_cells = ["B5","C5","D5","E5"]
+    for a_cell in list_of_cells:
+    # Add ability to clear cells after inserting
+        clear_cell_updates = worksht.cell(a_cell).value = ""
+    print("cleared cells")
+
+# TODO:
+# Add values to row and move entire row down
+#update_expense_row = worksht.append_table(values=['B5','C5','D5','E5'], start='B6', end=None, dimension='ROWS', overwrite=True, **kwargs)
+
+update_cells()
+clear_cell_updates()

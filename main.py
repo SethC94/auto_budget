be0a40ab91ad15af3fea1743d1a8aa6d3d403aff
Seth Creasman <sethcreasman@Seths-MacBook-Pro.local>
2024-02-28 13:05:33 -0800

    added append_and_drop_row function

--- main.py
+++ main.py
@@ -1,40 +1,34 @@
 # Importing required library
 import pygsheets
+import gspread
 import datetime
 
 # Create time/date
 today = datetime.date.today()
 # Create the Client
-client = pygsheets.authorize(service_account_file="/Users/sethcreasman/src/gcp/auto_budget/auto-budget-415622-767a226dbf8f.json")
+client = pygsheets.authorize(service_account_file="/Users/sethcreasman/src/gcp/sa_secrets/auto-budget-415622-670aa95d3ac5.json")
 
 # Open the doc and sheet for udpates
 spreadsht = client.open("auto_budget_sheet")
 worksht = spreadsht.worksheet("title", "Transactions")
 
-# Cell Values
-# expense_value =
-# expense_comment =
-# expense_category =
+# Test Cell Values
+## TODO: turn this into function args populated from text message etl
+expense_value = "5"
+expense_comment = "I updated this with python"
+expense_category = "House Payment"
+clear_cell_value = ""
 
 def update_cells():
     # #single cell value update for date amount and comment columns
-    update_date = worksht.cell("B5").set_text_format("bold", False).value = today.strftime('%d, %b %Y')
-    update_value = worksht.cell("C5").set_text_format("bold", False).value = "5"
-    update_comment = worksht.cell("D5").set_text_format("bold", False).value = "I updated this with Python"
-    update_category = worksht.cell("E5").set_text_format("bold", False).value = "House Payment"
-
+    update_date = worksht.cell("B5").set_text_format("bold", True).value = today.strftime('%d, %b %Y')
+    update_value = worksht.cell("C5").set_text_format("bold", True).value = expense_value
+    update_comment = worksht.cell("D5").set_text_format("bold", True).value = expense_comment
+    update_category = worksht.cell("E5").set_text_format("bold", True).value = expense_category
     print("added to cells")
 
-def clear_cell_updates():
-    list_of_cells = ["B5","C5","D5","E5"]
-    for a_cell in list_of_cells:
-    # Add ability to clear cells after inserting
-        clear_cell_updates = worksht.cell(a_cell).value = ""
-    print("cleared cells")
-
-# TODO:
-# Add values to row and move entire row down
-#update_expense_row = worksht.append_table(values=['B5','C5','D5','E5'], start='B6', end=None, dimension='ROWS', overwrite=True, **kwargs)
+def append_and_drop_row():
+    worksht.insert_rows(4,number=1)
 
 update_cells()
-clear_cell_updates()
+append_and_drop_row()
